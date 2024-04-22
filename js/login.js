139de2cf7ca50e4b67aca71754c2f08744f29478
chawnima <147719517+chawnima@users.noreply.github.com>
2024-04-23 00:03:16 +0700

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -4,10 +4,9 @@ document.getElementById("username").value = localStorage.getItem("username");
 document.getElementById("login").addEventListener("click", function () {
   username = document.getElementById("username").value;
   localStorage.setItem("username", document.getElementById("username").value);
-  if (document.getElementById("username").value){
-    window.location.href = "../sub/p1-form.html";
-  }
-  else{
+  if (document.getElementById("username").value) {
+    window.location.href = "../sub/p2-bill.html";
+  } else {
     alert("Username tidak terisi");
   }
 });
